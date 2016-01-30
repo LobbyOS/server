c3242d7dcb4eca0f962349ae5e2f60e53e1201a9
subins2000 <subins2000@gmail.com>
2016-01-30 18:46:13 +0530

    padding update as a suggestion of ana`

--- contents/apps/lobby-server/src/page/index.php
+++ contents/apps/lobby-server/src/page/index.php
@@ -24,7 +24,7 @@ $this->addScript("responsiveslides.min.js");
     </div>
   </div>
 </section>
-<section id="screenshots" class="even-section" style="padding: 10px 0;">
+<section id="screenshots" class="even-section" style="padding:70px 0 10px">
   <div class="container">
     <div class="row">
       <ul class="rslides">
